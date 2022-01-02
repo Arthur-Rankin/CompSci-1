a37dd5248e3405930e30794fa67a524254770e1a
arthur rankin smith <arthurrankin1992@gmail.com>
2022-01-01 21:23:27 -0600

    Update workspace.cpp

--- Chp7_arraysAndVectors/workspace.cpp
+++ Chp7_arraysAndVectors/workspace.cpp
@@ -1,89 +1,9 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
+#include <vector>
 using namespace std;
-
-int sum();
-double average();
-int highest();
-int lowest();
-
 
 int main()
 {
-    sum();
-    average();
-    highest();
-    lowest();
-
-    return 0;
-}
-
-int sum()
-{
-    const int NUM_UNITS = 24;
-    int units[NUM_UNITS];
-
-    int total = 0;
-    for(int i : units)
-        total += i;
-
-    return total;
-}
-
-double average()
-{
-    const int NUM_UNITS = 24;
-    int units[NUM_UNITS];
-
-    int total = 0;
-    double average;
-
-    for(int i : units)
-    {
-        total += i;
-    }
-  
-    average = total / NUM_UNITS;
-    return average;
-}
-
-int highest()
-{
-    const int SIZE = 50;
-    int numbers[SIZE];
-
-    int count;
-    int highest;
-
-    highest = numbers[0];
-    for(count = 0; count < SIZE; count++)
-    {
-        if (numbers[count] > highest)
-        {
-            highest = numbers[count];
-        }
-    }
-
-    return highest;
-}
-
-int lowest()
-{
-    const int SIZE = 50;
-    int numbers[SIZE];
-
-
-    int count;
-    int lowest;
-
-    lowest = numbers[0];
-    for(count = 0; count < SIZE; count++)
-    {
-        if(numbers[count] < lowest)
-        {
-            lowest = numbers[count];
-        }
-    }
-    return lowest;
+    
+   return 0;
 }
