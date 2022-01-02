f523a7bb14fb2bd4d3b1dcab140b45a00a7c0ad7
arthur rankin smith <arthurrankin1992@gmail.com>
2022-01-02 17:41:43 -0600

    playing with vectors in Chp7 work space

--- Chp7_arraysAndVectors/workspace.cpp
+++ Chp7_arraysAndVectors/workspace.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    
+    vector<int> nums;
+    for(int i = 0; i < 10; i++)
+    {
+        nums.push_back(i + 1);
+    };
+    for(int val : nums)
+    {
+        cout << nums[val];
+    };
    return 0;
 }
